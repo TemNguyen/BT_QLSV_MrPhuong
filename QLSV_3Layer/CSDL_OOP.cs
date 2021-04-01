7bb8b34513f6f20d4bde994a8d419d01903c5ec7
jthanh8144 <thanhvo618@gmail.com>
2021-04-01 21:53:19 +0700

    1st commit

---
+++ QLSV_3Layer/CSDL_OOP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV_3Layer
+{
+    class CSDL_OOP
+    {
+        public List<Student> ListStudent { get; set; }
+        public List<Class> ListClass { get; set; }
+        public static CSDL_OOP Instance 
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new CSDL_OOP();
+                }
+                return _Instance;
+            }
+            private set
+            {
+            }
+        }
+
+        private static CSDL_OOP _Instance;
+    }
+}
